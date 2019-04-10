a9f0a9267a9943c8277b3f485e28ffcd25be95f4
Thavananthan <nnanthu49@gmail.com>
2019-04-10 13:08:56 +0530

    them.js commit

---
+++ tourism-app-frontend/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Thems from'./component/them';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>HOME Page </h1>
+            <Thems/>
+      </div>
+    );
+  }
+}
+
+export default App;
