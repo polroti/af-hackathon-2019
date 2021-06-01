18e7fde46c878e6477292e29d41f7dae3e6b0386
Manoj Kumar <dharmakumar338@gmail.com>
2021-06-01 22:30:57 +0530

    code formatting

--- tourism-app-frontend/src/App.js
+++ tourism-app-frontend/src/App.js
@@ -1,61 +1,67 @@
-import React, { Component } from 'react';
-import {Navbar} from 'react-bootstrap';
-import {Nav} from 'react-bootstrap';
+import React, { Component } from "react";
+import { Navbar } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
 //import {NavDropdown} from 'react-bootstrap';
-import {FormControl} from 'react-bootstrap';
-import {Button} from 'react-bootstrap';
-import {Form} from 'react-bootstrap';
+import { FormControl } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 
-import {BrowserRouter as Router,Route,Link} from "react-router-dom";
-import Home from'./component/home';
-import AprilSpecial from'./component/special';
-import Plan from './component/planATrip';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./component/home";
+import AprilSpecial from "./component/special";
+import Plan from "./component/planATrip";
 
-
-
-
-
-
-import './App.css';
-
+import "./App.css";
 
 class App extends Component {
-
   render() {
+    return (
+      <Router>
+        <div>
+          <br />
+          <Navbar bg="primary" variant="dark">
+            <Navbar.Brand>
+              <h4>TRAVEL SRILANKA</h4>
+            </Navbar.Brand>
+            <Nav className="mr-auto">
+              <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+                <b>HOME</b>
+              </Link>
+              &nbsp;&nbsp;&nbsp;
+              <Link
+                to="/spacial"
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                <b>APRIL SPECIAL </b>
+              </Link>
+              &nbsp;&nbsp;&nbsp;
+              <Link
+                to="/plan"
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                <b>PLAN A TRIP </b>
+              </Link>
+              &nbsp;&nbsp;&nbsp;
+            </Nav>
+            <Form inline>
+              <FormControl
+                type="text"
+                placeholder="Search place"
+                className="mr-sm-2"
+              />
+              <Button variant="outline-light">Search</Button>
+            </Form>
+          </Navbar>
 
-          return(
-              <Router>
-              <div >
+          <br />
+        </div>
 
-
-                  <br />
-                  <Navbar bg="primary" variant="dark">
-                      <Navbar.Brand><h4>TRAVEL SRILANKA</h4></Navbar.Brand>
-                      <Nav className="mr-auto">
-                          <Link to="/" style={{textDecoration:'none', color:'white'}}><b>HOME</b></Link>&nbsp;&nbsp;&nbsp;
-                          <Link to="/spacial"style={{textDecoration:'none', color:'white'}} ><b>APRIL SPECIAL  </b></Link>&nbsp;&nbsp;&nbsp;
-
-                          <Link to="/plan"style={{textDecoration:'none', color:'white'}}  ><b>PLAN A TRIP  </b></Link>&nbsp;&nbsp;&nbsp;
-                      </Nav>
-                      <Form inline>
-                          <FormControl type="text" placeholder="Search place" className="mr-sm-2" />
-                          <Button variant="outline-light">Search</Button>
-                      </Form>
-                  </Navbar>
-
-                  <br />
-
-              </div>
-
-                  <Route path="/" exact component={Home}/>
-                  <Route path="/spacial"  component={AprilSpecial}/>
-                  <Route path="/plan"  component={Plan}/>
-
-              </Router>
-          );
-
-      }
-
+        <Route path="/" exact component={Home} />
+        <Route path="/spacial" component={AprilSpecial} />
+        <Route path="/plan" component={Plan} />
+      </Router>
+    );
+  }
 }
 
 export default App;
--- tourism-app-frontend/src/component/home.js
+++ tourism-app-frontend/src/component/home.js
@@ -1,58 +1,72 @@
-import React, { Component } from 'react';
-import Card from 'react-bootstrap/Card'
-import CardGroup from'react-bootstrap/CardGroup';
-import Button from 'react-bootstrap/Button';
-import foodImage from '../images/food.jpg';
-import img1 from'../images/image.jpg';
-import  img2 from '../images/images.jpg';
-import S8 from '../images/galle-ex.jpg';
-
+import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
+import Button from "react-bootstrap/Button";
+import foodImage from "../images/food.jpg";
+import img1 from "../images/image.jpg";
+import img2 from "../images/images.jpg";
+import S8 from "../images/galle-ex.jpg";
 
 export default class Home extends Component {
-
-    render() {
-        return(
-          <div>
-              <h1 align="center">WELCOME TO SRILANKA IN APRIL</h1><br/>
-
-             <center>< Card.Img src={S8} alt={"logo"} style={{width:"60%",height:"10%",}} /></center><br/>
-
-                <center><Button variant="primary">PLAN FOR APRIL</Button><br/></center><br/>
-
-              <CardGroup>
-                  <Card>
-                      <Card.Img variant="top" src={foodImage} style={{width:"100%"}} />
-                      <Card.Body>
-                          <Card.Title>Awurudu Foods in Sri Lanka</Card.Title>
-                          <Card.Text>
-                              This is a wider card with supporting text below as a natural lead-in to
-                              additional content. This content is a little bit longer.
-                          </Card.Text>
-                          <center><Button variant="primary">PLAN FOR APRIL</Button><br/></center><br/>
-                      </Card.Body>
-                      <Card.Footer>
-                          <small className="text-muted">Last updated 3 mins ago</small>
-                      </Card.Footer>
-                  </Card>
-                  <Card>
-                      <Card.Img variant="top" src={img1} style={{width:"100%"}} />
-                      <Card.Body>
-                          <Card.Title>Gangaramaya Holds its beauty</Card.Title>
-                          <Card.Text>
-                              This card has supporting text below as a natural lead-in to additional
-                              content.{' '}
-                          </Card.Text>
-                          <center><Button variant="primary">SEE MORE</Button><br/></center><br/>
-                      </Card.Body>
-                      <Card.Footer>
-                          <small className="text-muted">Last updated 3 mins ago</small>
-                      </Card.Footer>
-                  </Card>
-
-              </CardGroup>;
-          </div>
-        );
-    }
-
-
+  render() {
+    return (
+      <div>
+        <h1 align="center">WELCOME TO SRILANKA IN APRIL</h1>
+        <br />
+        <center>
+          <Card.Img
+            src={S8}
+            alt={"logo"}
+            style={{ width: "60%", height: "10%" }}
+          />
+        </center>
+        <br />
+        <center>
+          <Button variant="primary">PLAN FOR APRIL</Button>
+          <br />
+        </center>
+        <br />
+        <CardGroup>
+          <Card>
+            <Card.Img variant="top" src={foodImage} style={{ width: "100%" }} />
+            <Card.Body>
+              <Card.Title>Awurudu Foods in Sri Lanka</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+              <center>
+                <Button variant="primary">PLAN FOR APRIL</Button>
+                <br />
+              </center>
+              <br />
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src={img1} style={{ width: "100%" }} />
+            <Card.Body>
+              <Card.Title>Gangaramaya Holds its beauty</Card.Title>
+              <Card.Text>
+                This card has supporting text below as a natural lead-in to
+                additional content.{" "}
+              </Card.Text>
+              <center>
+                <Button variant="primary">SEE MORE</Button>
+                <br />
+              </center>
+              <br />
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+        </CardGroup>
+        ;
+      </div>
+    );
+  }
 }
